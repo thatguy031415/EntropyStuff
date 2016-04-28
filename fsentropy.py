603dc63c6455c4eba1bae920b2445138a468d330
Agent Cobra Bubbles <bbell031415@gmail.com>
2016-04-28 09:41:44 -0400

    Added files via upload

--- fsentropy.py
+++ fsentropy.py
@@ -5,45 +5,11 @@ import pefile
 if len(sys.argv) != 2:
     print "Usage: section_entropy.py [path]filename"
     sys.exit()
+filename = sys.argv[1]
 
-try:
-    filename = sys.argv[1]
-    pe = pefile.PE(filename)
-    print "This is a PE file.  Returning file and section entropy..."
+def section_entropy():
+    print filename + " is a PE file.  Returning file and section entropy..."
     # read the whole file into a byte array
-    f = open(sys.argv[1], "rb")
-    byteArr = map(ord, f.read())
-    f.close()
-    fileSize = len(byteArr)
-
-    '''Don't run for files over 500k; Above this size, entropy becomes less reliable as an indicator.'''
-    if fileSize >= 512000:
-        print "Maximum filesize exceeded.  Entropy is not a reliable indicator on files above 500kb."
-        sys.exit()
-
-    # calculate the frequency of each byte value in the file
-    freqList = []
-    for b in range(256):
-        ctr = 0
-        for byte in byteArr:
-            if byte == b:
-                ctr += 1
-        freqList.append(float(ctr) / fileSize)
-    # print freqList #used only for testing and debugging of freqList
-
-    # Shannon entropy
-    ent = 0.0
-    for freq in freqList:
-        if freq > 0:
-            ent = ent + freq * math.log(freq, 2)
-    ent = -ent
-    print 'Shannon entropy (min bits per byte-character):'
-    print ent
-    if ent >= 6.5:
-        print "File is likely packed or crypted."
-    else:
-        print "File is likely not packed or crypted."
-    # read the file sections, 1 at a time, into a byte array
     for section in pe.sections:
         byteArr = map(ord, section.get_data()[0:])
         sectionSize = len(byteArr)
@@ -77,8 +43,9 @@ try:
             print "Section " + section.Name + " is likely not packed or crypted."
         else:
             print "Section " + section.Name + " has very suspicious low entropy."
-except:
-    print "Not a PE File.  Returning file entropy only..."
+
+def file_entropy():
+
     # read the whole file into a byte array
     f = open(sys.argv[1], "rb")
     byteArr = map(ord, f.read())
@@ -112,3 +79,13 @@ except:
         print "File is likely packed or crypted."
     else:
         print "File is likely not packed or crypted."
+
+
+try:
+    pe = pefile.PE(filename)
+    section_entropy()
+    file_entropy()
+except:
+    print filename + " is not a PE File.  Returning file entropy only..."
+    file_entropy()
+
